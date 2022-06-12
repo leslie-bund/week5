7f481371175fe49ddd8b1b10f6ebf4e57811e035
leslie-bund <lchukwunweike@gmail.com>
2022-06-12 22:45:36 +0100

    Setup switch statement to handle requests

--- server/server/app.ts
+++ server/server/app.ts
@@ -4,10 +4,27 @@ implement your server code here
 */
 
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    if (req.method === "GET") {
-      res.end(JSON.stringify({ name: "hello" }));
+    // if (req.method === "GET") {
+    //   res.end(JSON.stringify({ name: "hello" }));
+    // }
+    switch (req.method) {
+      case 'GET' :
+        // Insert code here
+        break;
+      case 'POST' :
+        // Insert code here
+        break;
+      case 'PUT' :
+        // Insert code here
+        break;
+      case 'DELETE' :
+        // Insert code here
+        break;
+      default: 
+        console.log('Request is not accounted for within this API');
+        break;
     }
   }
 );
 
-server.listen(3005);
+server.listen(3005, ()=> console.log('Server is running'));
