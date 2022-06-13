f1ade8e9c0a1fd20036e1ec11101ca51fabd9793
leslie-bund <lchukwunweike@gmail.com>
2022-06-13 10:55:46 +0100

    Modified models to create database json file dynamically

--- server/server/Models/model.ts
+++ server/server/Models/model.ts
@@ -1,4 +1,20 @@
-import data from './data.json';
+// import data from './data.json';
+import path from 'path';
+import fs from 'fs/promises';
+
+const dbFilePath = path.resolve(__dirname, 'data.json');
+const data = (async function getFileContent() {
+    try {
+        const content = await fs.readFile(dbFilePath, {encoding: 'utf8'});
+        return content;
+    } catch (error) {
+        const firstContent = JSON.stringify([]);
+        await fs.writeFile(dbFilePath, firstContent);
+        const content = await fs.readFile(dbFilePath, {encoding: 'utf8'});
+        return content;
+    }
+})();
+
 
 export function findAll() {
     return new Promise((resolve, reject) => {
