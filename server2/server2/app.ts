2b86d6cc6e0ac93249a1ea1c887daa534fdda192
leslie-bund <lchukwunweike@gmail.com>
2022-06-14 21:51:08 +0100

    Imported and implemented html-metadata-parser

--- server2/server2/app.ts
+++ server2/server2/app.ts
@@ -1,12 +1,26 @@
 import http, { IncomingMessage, Server, ServerResponse } from 'http';
+import https from 'https';
+import { parser } from 'html-metadata-parser';
+
 /*
 implement your server code here
 */
 
-const server: Server = http.createServer(
-  (req: IncomingMessage, res: ServerResponse) => {
+const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+    const urlObject = new URL(<string>req.url, `http://${req.headers.host}`);
     if (req.method === "GET") {
-      res.end(JSON.stringify({ name: "hello" }));
+      (async () => {
+        var result = await parser(`https:/` + urlObject.pathname);
+        console.log(JSON.stringify(result, ['og'],3));
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ 
+          meta: {
+            title: result?.meta?.title || '',
+            description: result?.meta?.description || ''
+          },
+          images: result.images,
+        }));
+      })();
     }
   }
 );
